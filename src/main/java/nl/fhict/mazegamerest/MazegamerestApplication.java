853c08bb051d64f96af526d176d2c7cc8155829b
MarkTempelman <43167879+MarkTempelman@users.noreply.github.com>
2020-12-08 15:03:55 +0100

    Project created

---
+++ src/main/java/nl/fhict/mazegamerest/MazegamerestApplication.java
@@ -0,0 +1,13 @@
+package nl.fhict.mazegamerest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazegamerestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazegamerestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fhict/mazegamerest/MazegamerestApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fhict.mazegamerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazegamerestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
